7dcc6391fc5a674a1efae445eea7c882d1d009bc
Daniel Denton <daniel.j.denton@gmail.com>
2021-07-31 22:11:40 -0400

    Add support to find files of arbitrary types

--- offset_tweak.py
+++ offset_tweak.py
@@ -1,16 +1,15 @@
 import os
 import re
 
-ssc_regex = re.compile('.*\.ssc')
 
-
-def filewalk(root_directory, regex):
+def filewalk(root_directory, filetype='ssc'):
+    filetype_regex = re.compile(f'.*\.{filetype}')
     for dirpath, dirs, files in os.walk(root_directory):
         # print(f'dirpath={dirpath} dirs={dirs} files={files}')
         for file in files:
-            if regex.match(file):
+            if filetype_regex.match(file):
                 print(f'{dirpath}/{file}')
 
 
 if __name__ == '__main__':
-    filewalk('5guys1pack', ssc_regex)
+    filewalk('5guys1pack')
